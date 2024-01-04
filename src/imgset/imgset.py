fb53603cdc868960b51089cdc9635d5355b307d0
fbbcool <fbbcool@gmail.com>
2024-01-04 16:01:57 +0100

    .

--- src/imgset/imgset.py
+++ src/imgset/imgset.py
@@ -1,3 +1,15 @@
+import os
+from pathlib import Path
+from typing import Final
+
 class ImgSet:
-    def __init__(self) -> None:
-        self.sepp = 7
+    TEST_PATH_CRAWL : Final = "test/imgs"
+    def __init__(self, dir: str) -> None:
+        self.path_crawl = dir
+
+    def _initialze(self):
+        paths = []
+        dirs = [dir[0] for dir in os.walk(self.path_crawl)]
+        for dir in dirs:
+            path = Path(dir)
+            paths += list(set(path.glob("*.jpg")))
