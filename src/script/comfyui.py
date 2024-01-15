f00cfc97fc92efaf64a45d655bf57546ed42fc7b
fbbcool <fbbcool@gmail.com>
2024-01-15 22:22:52 +0100

    .

--- src/script/comfyui.py
+++ src/script/comfyui.py
@@ -94,7 +94,7 @@ class ModelInstComfyUi:
             #ModelInst(t, ModelType.Lora, DownloadMethod.GDrive, "1WOAizZJY4g4qO8nGWOW2UuMLvbI3bWsM", "lara"),
             ModelInst(t, ModelType.Lora, DownloadMethod.Wget, "https://drive.usercontent.google.com/download?id=1WOAizZJY4g4qO8nGWOW2UuMLvbI3bWsM&export=download&authuser=0&confirm=t&uuid=2754e049-8f00-4b61-af55-974da0d86cf4&at=APZUnTVuxLuA5FGY9HgGSYyqScEe%3A1705351998875", "lara"),
             ModelInst(t, ModelType.Lora, DownloadMethod.Wget, "https://civitai.com/api/download/models/260383?type=Model&format=SafeTensor", "Muscle_bimbo"),
-            ModelInst(t, ModelType.ClipVision, DownloadMethod.Hugging, "InvokeAI/ip_adapter_sd_image_encoder/model.safetensors", "ip_adapter_sd_image_encoder"),
+            ModelInst(t, ModelType.ClipVision, DownloadMethod.Wget, "https://huggingface.co/InvokeAI/ip_adapter_sd_image_encoder/resolve/main/model.safetensors?download=true", "ip_adapter_sd_image_encoder"),
             ModelInst(t, ModelType.IPAdapter, DownloadMethod.Wget, "https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter_sd15.safetensors", "ip-adapter_sd15"),
             ModelInst(t, ModelType.IPAdapter, DownloadMethod.Wget, "https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter_sd15_light.safetensors", "ip-adapter_sd15_light"),
             ModelInst(t, ModelType.IPAdapter, DownloadMethod.Wget, "https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter-plus_sd15.safetensors", "ip-adapter-plus_sd15"),
