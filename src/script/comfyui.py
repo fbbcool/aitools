a55bb7df205722a37d4696997ed752fde5d2f0ab
fbbcool <fbbcool@gmail.com>
2024-01-17 00:44:24 +0100

    tile

--- src/script/comfyui.py
+++ src/script/comfyui.py
@@ -102,7 +102,7 @@ class ModelInstComfyUi:
             ModelInst(t, ModelType.IPAdapter, DownloadMethod.Wget, "https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter-full-face_sd15.safetensors", "ip-adapter-full-face_sd15"),
             ModelInst(t, ModelType.IPAdapter, DownloadMethod.Wget, "https://huggingface.co/h94/IP-Adapter/resolve/main/models/ip-adapter_sd15_vit-G.safetensors", "ip-adapter_sd15_vit-G"),
             ModelInst(t, ModelType.Controlnet, DownloadMethod.Wget, "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors?download=true", "control_depth-fp16"),
-            ModelInst(t, ModelType.Controlnet, DownloadMethod.Wget, "https://civitai.com/api/download/models/64266", "control_tiles-fp16"),
+            ModelInst(t, ModelType.Controlnet, DownloadMethod.Wget, "https://huggingface.co/lllyasviel/control_v11f1e_sd15_tile/resolve/main/diffusion_pytorch_model.bin?download=true", "control_tiles-fp16"),
         ]
         for model in models:
             model.install
