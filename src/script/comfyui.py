e3d832532733901c88be3d35a285517feba8e307
fbbcool <fbbcool@gmail.com>
2024-05-01 18:38:14 +0200

    +chloe

--- src/script/comfyui.py
+++ src/script/comfyui.py
@@ -160,6 +160,7 @@ class ModelInstComfyUi:
             #ModelInst(t, ModelType.Lora, wget, "https://civitai.com/api/download/models/281780?type=Model&format=SafeTensor", "Muscle_mgm"),
             ModelInst(t, ModelType.Lora, wget, "https://civitai.com/api/download/models/87153?type=Model&format=SafeTensor", "Adetailer"),
             ModelInst(t, ModelType.Lora, wget, "https://civitai.com/api/download/models/96699", "Minigiantess"),
+            ModelInst(t, ModelType.Lora, wget, "https://civitai.com/api/download/models/154604", "ChloeVevrier"),
             #ModelInst(t, ModelType.Lora, wget, "https://civitai.com/api/download/models/295786", "MuscleGirlsXL"),
             ModelInst(t, ModelType.Lora, wget, "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid_sd15_lora.safetensors"),
             ModelInst(t, ModelType.Lora, wget, "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid-plusv2_sd15_lora.safetensors"),
@@ -259,22 +260,22 @@ if __name__ == "__main__":
 # https://huggingface.co/lllyasviel/ControlNet-v1-1/tree/main #ctrlnet models
 #https://huggingface.co/h94/IP-Adapter-FaceID/tree/main
 # custom nodes
-# 002 - ComfyUI-Impact-Pack
-# 003 - ComfyUI-Inspire-Pack
-# 006 - controlnet
+# 002 - ComfyUI-Impact-Pack *
+# 003 - ComfyUI-Inspire-Pack *
+# 006 - controlnet *
 # 013 - clipseg
 # 014 - ComFyUI Cutoff
 # 016 - ComFyUI Noise
-# 017 - Efficiency nodes 2.0+
-# 061 - ComfyUI WD 1.4 Tagger
-# 065 - ultimateSDupscale
-# 090 - ipadapter plus
-# 127 - reactor
-# 157 - rgthree 
+# 017 - Efficiency nodes 2.0+ *
+# 061 - ComfyUI WD 1.4 Tagger *
+# 065 - ultimateSDupscale *
+# 090 - ipadapter plus *
+# 127 - ReActor Node for ComfyUI
+# 157 - rgthree *
 # 202 - comfy_PoP
-# 215 - Essentials
+# 215 - Essentials *
 # 227 - CFG Scale Fix
 # 231 - Use Everywher
 # 258 - segment anything
 # 273 - mask bounding box
-# 382 - Comfyui Easy USe
+# 382 - Comfyui Easy Use *
