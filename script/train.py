aeea47b97bf62dfe1ac87741617f1e96976d742c
fbbcool <fbbcool@gmail.com>
2025-07-14 17:36:37 +0200

    fix

--- script/train.py
+++ script/train.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from aidb.trainer import TrainerKohya
 
 folder_train = TrainerKohya.ROOT
-from_file = Path(TrainerKohya.filename_config)
-to_file = folder_train / TrainerKohya.filename_config
+from_file = Path(TrainerKohya.filename_config())
+to_file = folder_train / TrainerKohya.filename_config()
 # copy train config to train folder
 shutil.copy(str(from_file), str(to_file))
 
