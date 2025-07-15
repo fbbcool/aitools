5e788a89d90b454ad32d725d1024bf33819f3523
fbbcool <fbbcool@gmail.com>
2025-07-15 07:39:08 +0200

    .

--- script/train.py
+++ script/train.py
@@ -1,11 +1,2 @@
-import shutil
-from pathlib import Path
 from aidb.trainer import TrainerKohya
-
-folder_train = TrainerKohya.ROOT
-from_file = Path(TrainerKohya.FILENAME_CONFIG)
-to_file = folder_train / TrainerKohya.FILENAME_CONFIG
-# copy train config to train folder
-shutil.copy(str(from_file), str(to_file))
-
-TrainerKohya(folder_train)
+TrainerKohya()
