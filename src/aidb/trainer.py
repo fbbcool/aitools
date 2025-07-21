9a3845647d2b023eef8c60ffbf2ea3221a8b4603
fbbcool <fbbcool@gmail.com>
2025-07-21 10:00:18 +0200

    .

--- src/aidb/trainer.py
+++ src/aidb/trainer.py
@@ -1,15 +1,20 @@
 import json
+import os
 from pathlib import Path
 import shutil
+import sys
+import time
 from typing import Final
+from urllib.parse import parse_qs, unquote, urlparse
 
 from huggingface_hub import hf_hub_download
+import urllib
 
 from aidb.hfdataset import HFDatasetImg
 
 class TrainerKohya:
-    ROOT: Final = Path("/workspace/train")
-    #ROOT: Final = Path("/Volumes/data/Project/AI/REPOS/aitools/build/train")
+    #ROOT: Final = Path("/workspace/train")
+    ROOT: Final = Path("/Volumes/data/Project/AI/REPOS/aitools/build/train")
     FILENAME_CONFIG: Final = "config_trainer.json"
     FILE_CONFIG: Final = ROOT / FILENAME_CONFIG
     FOLDER_DATASET: Final = ROOT / "dataset"
@@ -18,6 +23,8 @@ class TrainerKohya:
     FILE_SAMPLE_PROMPTS: Final = ROOT / "sample_prompts.txt"
     FILE_CONFIG_DATASET: Final = ROOT / "dataset.toml"
     FILE_TRAIN_SCRIPT: Final = ROOT / "train.sh"
+    CHUNK_SIZE: Final = 1638400
+    USER_AGENT: Final = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
     def __init__(self, load_models: bool = True) -> None:
         self._config: dict = {}
@@ -28,7 +35,7 @@ class TrainerKohya:
         self._ids_used = []
         self._batch_size = 1
 
-        self._hfdl_ckpt: tuple[str,str] = ("black-forest-labs/FLUX.1-dev", "flux1-dev.safetensors")
+        self._hfdl_ckpt: tuple[str,str] | None = ("black-forest-labs/FLUX.1-dev", "flux1-dev.safetensors")
         self._hfdl_vae: tuple[str,str] = ("black-forest-labs/FLUX.1-dev", "ae.safetensors")
         self._hfdl_clipl: tuple[str,str] = ("comfyanonymous/flux_text_encoders", "clip_l.safetensors")
         self._hfdl_t5xxl: tuple[str,str] = ("comfyanonymous/flux_text_encoders", "t5xxl_fp16.safetensors")
@@ -36,6 +43,8 @@ class TrainerKohya:
         self._file_vae: str = ""
         self._file_clipl: str = ""
         self._file_t5xxl: str = ""
+        
+        self._caidl_ckpt: tuple[str,str] | None = None
         
         # read config file with pathlib and store the dict
         try:
@@ -50,6 +59,17 @@ class TrainerKohya:
         self._trigger = self._config["trigger"]
         self._name = self._config["name"]
         self._batch_size = self._config["batch_size"]
+
+        # if model key doesnt exist do nothing
+        if "model" in self._config:
+            model = self._config["model"]
+            self._hfdl_ckpt = None
+            self._caidl_ckpt = None
+            if "hf" in model:
+                self._hfdl_ckpt = (model["hf"]["repo_id"], model["hf"]["file"])
+            elif "cai" in model:
+                self._caidl_ckpt = (model["cai"]["url"], "")
+
 
 
         # load HF dataset
@@ -81,7 +101,10 @@ class TrainerKohya:
 
 
     def _download_models(self, force: bool = False) -> None:
-        self._file_ckpt = hf_hub_download(repo_id=self._hfdl_ckpt[0], filename=self._hfdl_ckpt[1], cache_dir=self.FOLDER_MODELS)
+        if self._hfdl_ckpt:
+            self._file_ckpt = hf_hub_download(repo_id=self._hfdl_ckpt[0], filename=self._hfdl_ckpt[1], cache_dir=self.FOLDER_MODELS)
+        elif self._caidl_ckpt:
+            self._file_ckpt = self._download_civitai(self._caidl_ckpt[0], self.FOLDER_MODELS)
         self._file_clipl = hf_hub_download(repo_id=self._hfdl_clipl[0], filename=self._hfdl_clipl[1], cache_dir=self.FOLDER_MODELS)
         self._file_t5xxl = hf_hub_download(repo_id=self._hfdl_t5xxl[0], filename=self._hfdl_t5xxl[1], cache_dir=self.FOLDER_MODELS)
         self._file_vae = hf_hub_download(repo_id=self._hfdl_vae[0], filename=self._hfdl_vae[1], cache_dir=self.FOLDER_MODELS)
@@ -227,3 +250,93 @@ accelerate launch \\
             print(f"Config successfully saved to {configfile}")
         except Exception as e:
             print(f"Error saving config to JSON: {e}")
+    
+    def _download_civitai(self, url: str, ofolder: Path) -> Path:
+        opath = str(ofolder)
+        # get token from env TOKEN_CIVITAI
+        #token = os.environ.get("CAI_TOKEN", "")
+        token = os.environ.get("CAI_TOKEN", "2c1cdcc01625085bff329ba907d64948")
+        
+
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'User-Agent': self.USER_AGENT,
+        }
+
+        # Disable automatic redirect handling
+        class NoRedirection(urllib.request.HTTPErrorProcessor):
+            def http_response(self, request, response):
+                return response
+            https_response = http_response
+
+        request = urllib.request.Request(url, headers=headers)
+        opener = urllib.request.build_opener(NoRedirection)
+        response = opener.open(request)
+
+        if response.status in [301, 302, 303, 307, 308]:
+            redirect_url = response.getheader('Location')
+
+            # Extract filename from the redirect URL
+            parsed_url = urlparse(redirect_url)
+            query_params = parse_qs(parsed_url.query)
+            content_disposition = query_params.get('response-content-disposition', [None])[0]
+
+            if content_disposition:
+                filename = unquote(content_disposition.split('filename=')[1].strip('"'))
+            else:
+                raise Exception('Unable to determine filename')
+
+            response = urllib.request.urlopen(redirect_url)
+        elif response.status == 404:
+            raise Exception('File not found')
+        else:
+            raise Exception('No redirect found, something went wrong')
+
+        total_size = response.getheader('Content-Length')
+
+        if total_size is not None:
+            total_size = int(total_size)
+
+        output_file = os.path.join(opath, filename)
+
+        with open(output_file, 'wb') as f:
+            downloaded = 0
+            start_time = time.time()
+
+            while True:
+                chunk_start_time = time.time()
+                buffer = response.read(self.CHUNK_SIZE)
+                chunk_end_time = time.time()
+
+                if not buffer:
+                    break
+
+                downloaded += len(buffer)
+                f.write(buffer)
+                chunk_time = chunk_end_time - chunk_start_time
+
+                if chunk_time > 0:
+                    speed = len(buffer) / chunk_time / (1024 ** 2)  # Speed in MB/s
+
+                if total_size is not None:
+                    progress = downloaded / total_size
+                    sys.stdout.write(f'\rDownloading: {filename} [{progress*100:.2f}%] - {speed:.2f} MB/s')
+                    sys.stdout.flush()
+
+        end_time = time.time()
+        time_taken = end_time - start_time
+        hours, remainder = divmod(time_taken, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        if hours > 0:
+            time_str = f'{int(hours)}h {int(minutes)}m {int(seconds)}s'
+        elif minutes > 0:
+            time_str = f'{int(minutes)}m {int(seconds)}s'
+        else:
+            time_str = f'{int(seconds)}s'
+
+        sys.stdout.write('\n')
+        print(f'Download completed. File saved as: {filename}')
+        print(f'Downloaded in {time_str}')
+
+        return self.FOLDER_MODELS / filename
