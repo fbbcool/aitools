8549e2b132aa9f20ac524d8dad4072880df87f55
fbbcool <fbbcool@gmail.com>
2025-07-27 16:13:01 +0200

    .

--- src/script/comfyui.py
+++ src/script/comfyui.py
@@ -394,7 +394,7 @@ class ModelInstComfyUi:
             ModelInst(t, ModelType.Clip, hf2, "Aitrepreneur/FLX", "umt5-xxl-encoder-Q5_K_S.gguf", ""),
 
             #clip vision
-            ModelInst(t, ModelType.ClipVision, hf2, "Kijai/WanVideo_comfy", "open-clip-xlm-roberta-large-vit-huge-14_visual_fp16", ""),
+            ModelInst(t, ModelType.ClipVision, hf2, "Kijai/WanVideo_comfy", "open-clip-xlm-roberta-large-vit-huge-14_visual_fp16.safetensors", ""),
             
             # lora
             ModelInst(t, ModelType.Lora, hf2, "Aitrepreneur/FLX", "Wan2.1_T2V_14B_FusionX_LoRA.safetensors", ""),
