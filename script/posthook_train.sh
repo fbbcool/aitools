bffe41c87701da4d2e0ee466054233a546f1803e
fbbcool <fbbcool@gmail.com>
2025-08-04 12:03:06 +0200

    .

--- script/posthook_train.sh
+++ script/posthook_train.sh
@@ -2,3 +2,4 @@ git clone https://github.com/tdrussell/diffusion-pipe /workspace/diffusion-pipe
 cd /workspace/diffusion-pipe
 git submodule update --init --recursive
 pip install -r requirements.txt
+cp /workspace/train/train.sh /workspace/diffusion-pipe
