5fc9ffe0f9c3d40805d83a79a6b5fe1cd3a7b775
fbbcool <fbbcool@gmail.com>
2025-08-10 14:05:35 +0200

    .

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -38,8 +38,6 @@ class Trainer:
     FILE_CONFIG_DIFFPIPE: Final = ROOT / "diffpipe.toml"
     FILE_CONFIG_DATASET: Final = ROOT / "dataset.toml"
     FILE_TRAIN_SCRIPT: Final = ROOT / "train.sh"
-    CHUNK_SIZE: Final = 1638400
-    USER_AGENT: Final = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
     def __init__(self, repo_id_hfd: str, load_models: bool = True, cache_full_dataset: bool = False, multithread: bool = False, caption_missing=False, caption_force=False) -> None:
         self._repo_id_hfd = repo_id_hfd
@@ -57,24 +55,6 @@ class Trainer:
         self._num_repeats = 1
         self._ids_img: list[str] = []
 
-        ##self._hfdl_ckpt: tuple[str,str] | None = ("black-forest-labs/FLUX.1-dev", "flux1-dev.safetensors")
-        #self._hfdl_ckpt_flux: tuple[str,str] | None = (self.MODEL_REPO_ID_FLUX, self.MODEL_FILE_FLUX)
-        #self._hfdl_vae_flux: tuple[str,str] = ("black-forest-labs/FLUX.1-dev", "ae.safetensors")
-        #self._hfdl_clipl_flux: tuple[str,str] = ("comfyanonymous/flux_text_encoders", "clip_l.safetensors")
-        #self._hfdl_llm_flux: tuple[str,str] = ("comfyanonymous/flux_text_encoders", "t5xxl_fp16.safetensors")
-        ##self._hfdl_t5xxl: tuple[str,str] = ("comfyanonymous/flux_text_encoders", "t5xxl_fp8_e4m3fn_scaled.safetensors")
-        
-        #self._hfdl_ckpt_wan: tuple[str,str] | None = (self.MODEL_REPO_ID_BASE, self.MODEL_FILE_WAN)
-        #self._hfdl_llm_wan: tuple[str,str] = ("Kijai/WanVideo_comfy", "umt5-xxl-enc-fp8_e4m3fn.safetensors")
-        
-        #self._file_ckpt: str = ""
-        #self._file_vae: str = ""
-        #self._file_transformer: str = ""
-        #self._file_llm: str = ""
-        #self._folder_base_model: str = "" # base ckpt full checkout w/o models, only config (e.eg. for WAN)
-        
-        #self._caidl_ckpt: tuple[str,str] | None = None
-        
         # make root folder
         self.ROOT.mkdir(exist_ok=True)
         
@@ -109,7 +89,7 @@ class Trainer:
             return
         _base_models_all: dict = self._config_base.get("models", {})
         self._models: dict[str, dict] | None = _base_models_all.get(self._type, None)
-        self._model_links: dict[str, str]
+        self._model_links: dict[str, str] = {}
 
         #
         # load dataset
@@ -527,95 +507,3 @@ NCCL_P2P_DISABLE="1" NCCL_IB_DISABLE="1" deepspeed --num_gpus=1 train.py --deeps
             f.write(str_file)
         self.FILE_TRAIN_SCRIPT.chmod(0o777)
     
-
-    def _download_civitai(self, url: str) -> Path:
-        # get token from env TOKEN_CIVITAI
-        token = os.environ.get("CAI_TOKEN", "")
-        
-
-        headers = {
-            'Authorization': f'Bearer {token}',
-            'User-Agent': self.USER_AGENT,
-        }
-
-        # Disable automatic redirect handling
-        class NoRedirection(urllib.request.HTTPErrorProcessor):
-            def http_response(self, request, response):
-                return response
-            https_response = http_response
-
-        request = urllib.request.Request(url, headers=headers)
-        opener = urllib.request.build_opener(NoRedirection)
-        response = opener.open(request)
-
-        if response.status in [301, 302, 303, 307, 308]:
-            redirect_url = response.getheader('Location')
-
-            # Extract filename from the redirect URL
-            parsed_url = urlparse(redirect_url)
-            query_params = parse_qs(parsed_url.query)
-            content_disposition = query_params.get('response-content-disposition', [None])[0]
-
-            if content_disposition:
-                filename = unquote(content_disposition.split('filename=')[1].strip('"'))
-            else:
-                raise Exception('Unable to determine filename')
-
-            response = urllib.request.urlopen(redirect_url)
-        elif response.status == 404:
-            raise Exception('File not found')
-        else:
-            raise Exception('No redirect found, something went wrong')
-
-        total_size = response.getheader('Content-Length')
-
-        if total_size is not None:
-            total_size = int(total_size)
-
-        output_file = self.FOLDER_MODELS / filename
-
-        if output_file.exists():
-            print(f"\t already downloaded: {output_file.name}")
-            return output_file
-
-        with output_file.open('wb') as f:
-            downloaded = 0
-            start_time = time.time()
-
-            while True:
-                chunk_start_time = time.time()
-                buffer = response.read(self.CHUNK_SIZE)
-                chunk_end_time = time.time()
-
-                if not buffer:
-                    break
-
-                downloaded += len(buffer)
-                f.write(buffer)
-                chunk_time = chunk_end_time - chunk_start_time
-
-                if chunk_time > 0:
-                    speed = len(buffer) / chunk_time / (1024 ** 2)  # Speed in MB/s
-
-                if total_size is not None:
-                    progress = downloaded / total_size
-                    sys.stdout.write(f'\rDownloading: {filename} [{progress*100:.2f}%] - {speed:.2f} MB/s')
-                    sys.stdout.flush()
-
-        end_time = time.time()
-        time_taken = end_time - start_time
-        hours, remainder = divmod(time_taken, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        if hours > 0:
-            time_str = f'{int(hours)}h {int(minutes)}m {int(seconds)}s'
-        elif minutes > 0:
-            time_str = f'{int(minutes)}m {int(seconds)}s'
-        else:
-            time_str = f'{int(seconds)}s'
-
-        sys.stdout.write('\n')
-        print(f'Download completed. File saved as: {filename}')
-        print(f'Downloaded in {time_str}')
-
-        return output_file
