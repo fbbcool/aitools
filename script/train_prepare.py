e5f1f2d420009eec029963708332b4d44d5ca863
fbbcool <fbbcool@gmail.com>
2025-08-16 12:30:39 +0200

    .

--- script/train_prepare.py
+++ script/train_prepare.py
@@ -1,3 +1,3 @@
 from trainer import Trainer
 repo_id="fbbcool/1gts_03_1k"
-Trainer(repo_id, type_model="wan21", load_models=True, cache_full_dataset=True, multithread=True, caption_missing=False)
+Trainer(repo_id, type_model="wan222_high", load_models=True, cache_full_dataset=True, multithread=True, caption_missing=False)
