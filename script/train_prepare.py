a2dfe9c383b2d46ddc32fd66f6c5f758447c0fd7
fbbcool <fbbcool@gmail.com>
2025-08-18 21:48:46 +0200

    .

--- script/train_prepare.py
+++ script/train_prepare.py
@@ -1,3 +1,3 @@
 from trainer import Trainer
-repo_ids=["fbbcool/1gts_03_1k", "fbbcool/1man"]
+repo_ids=["fbbcool/1gts_03_1k", "fbbcool/1man", "fbbcool/1fem"]
 Trainer(repo_ids, type_model="wan22_high", multithread=True)
