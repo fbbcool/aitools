061f0370c70a39947c342b7e80a005ae107a84bf
fbbcool <fbbcool@gmail.com>
2025-08-18 14:33:34 +0200

    .

--- script/caption.py
+++ script/caption.py
@@ -4,6 +4,7 @@ from aidb.hfdataset import HFDatasetImg
 caper = CapJoy(trigger="1man")
 #hfd = HFDatasetImg(repo_id="fbbcool/gts01_r35")
 hfd = HFDatasetImg(repo_id="fbbcool/1man", force_meta_dl=True)
+hfd.cache()
 
 n = len(hfd)
 error = 0
