f46ab93789e4e9561d9969d4a64cca689759e6e2
fbbcool <fbbcool@gmail.com>
2025-08-21 16:02:31 +0200

    .

--- src/aidb/caption_joy.py
+++ src/aidb/caption_joy.py
@@ -20,6 +20,7 @@ CONTENT_SYSTEM: Final = {
 	"1hairy": "The users are interested in woman with very hairy pussies and very hairy asses how they present their bodies.",
 	"1pussy_insert": "a giantess woman is inserting a small adult man into her vagina. if you want to use 'the small man is positioned between her legs', use 'inserted in her vagina' instead and describe wether the lower or upper bodypart of the small man is inserted and not visible.",
 	"1man": "The image is about a small man with an erect penis on a grey background. describe the man as a 'small man'. if the small man takes most of the image, call it a closeup, if not, call it a photograph.",
+	"1legsemp": "The image is tall giantess woman with muscular legs. describe the woman as a 'giantess woman'. they all have muscular legs and calves",
 }
 DEFAULT_PROMPT: Final = "Write a very long detailed description for this image."
 CONTENT_PROMPT: Final = {
@@ -31,6 +32,7 @@ CONTENT_PROMPT: Final = {
 	"1hairy": "The users are interested in woman with very hairy pussies and very hairy asses how they present their bodies.",
 	"1pussy_insert": "a giantess woman is inserting a small adult man into her vagina. if you want to use 'the small man is positioned between her legs', use 'inserted in her vagina' instead and describe wether the lower or upper bodypart of the small man is inserted and not visible.",
 	"1man": "The image is about a small man with an erect penis on a grey background. describe the man as a 'small man'. if the small man takes most of the image, call it a closeup, if not, call it a photograph.",
+	"1legsemp": "The image is tall giantess woman with muscular legs. describe the woman as a 'giantess woman'. they all have muscular legs and calves",
 }
 
 class CapJoy():
