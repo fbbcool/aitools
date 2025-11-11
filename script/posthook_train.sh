5f9ee0fe2c873a772679b48336fcee5185a39e90
mikemoik <mikemoik@gmail.com>
2025-11-11 19:56:49 +0100

    .

--- script/posthook_train.sh
+++ script/posthook_train.sh
@@ -3,7 +3,8 @@ cd /workspace/diffusion-pipe
 git submodule update --init --recursive
 cp /workspace/train/train.sh /workspace/diffusion-pipe
 
-echo -n " !!! !!!!!!!!!!!!!!!!!!!!!!!!!! !!!"
-echo -n " !!! update to flash-attn 2.8.3 !!!"
-echo -n " !!! !!!!!!!!!!!!!!!!!!!!!!!!!! !!!"
-pip install -r requirements.txt --upgrade --no-build-isolation
+#echo -n " !!! !!!!!!!!!!!!!!!!!!!!!!!!!! !!!"
+#echo -n " !!! update to flash-attn 2.8.3 !!!"
+#echo -n " !!! !!!!!!!!!!!!!!!!!!!!!!!!!! !!!"
+#pip install -r requirements.txt --upgrade --no-build-isolation
+pip install -r requirements.txt
