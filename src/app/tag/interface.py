4d5afc7ccd39582cc27d867f845a55da6311b965
mikemoik <mikemoik@gmail.com>
2025-11-11 16:33:02 +0100

    from aitools driect

--- src/app/tag/interface.py
+++ src/app/tag/interface.py
@@ -17,42 +17,99 @@ LABEL_BUTT_GALLERY_OFF: Final = "Show Tags Gallery"
 LABEL_BUTT_GALLERY_ON_NOTAGS: Final = "Hide No Tags Gallery"
 LABEL_BUTT_GALLERY_OFF_NOTAGS: Final = "Show No Tags Gallery"
 
-img_empty = Image.new('RGB', (1, 1))
-rows_galleries = [gr.Gallery(
-    type="pil",
-    preview=True,
-    value=[img_empty],
-    scale=1,
-    columns=5,
-    rows=2,
-    height=300,
-    selected_index=0,
-    show_download_button=False,
-    show_share_button=False,
-    visible=False,
-    elem_id=f"gallery_{i}"
-    ) for i in range(TAG_NUM_PER_PAGE)]
-rows_butt_galleries = [gr.Button(LABEL_BUTT_GALLERY_OFF,value=LABEL_BUTT_GALLERY_OFF, scale=1,visible=True, elem_id=f"but-gallery_{i}") for i in range(TAG_NUM_PER_PAGE)]
-rows_butt_galleries_notags = [gr.Button(LABEL_BUTT_GALLERY_OFF_NOTAGS,value=LABEL_BUTT_GALLERY_OFF_NOTAGS, scale=1,visible=True, elem_id=f"but-gallery_{i}") for i in range(TAG_NUM_PER_PAGE)]
-rows_tag = [gr.Textbox(value="",label="Tag",placeholder="empty", visible=True, elem_id=f"tag_{i}", interactive=False) for i in range(TAG_NUM_PER_PAGE)]
+img_empty = Image.new("RGB", (1, 1))
+rows_galleries = [
+    gr.Gallery(
+        type="pil",
+        preview=True,
+        value=[img_empty],
+        scale=1,
+        columns=5,
+        rows=2,
+        height=300,
+        selected_index=0,
+        show_download_button=False,
+        show_share_button=False,
+        visible=False,
+        elem_id=f"gallery_{i}",
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
+rows_butt_galleries = [
+    gr.Button(
+        LABEL_BUTT_GALLERY_OFF,
+        value=LABEL_BUTT_GALLERY_OFF,
+        scale=1,
+        visible=True,
+        elem_id=f"but-gallery_{i}",
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
+rows_butt_galleries_notags = [
+    gr.Button(
+        LABEL_BUTT_GALLERY_OFF_NOTAGS,
+        value=LABEL_BUTT_GALLERY_OFF_NOTAGS,
+        scale=1,
+        visible=True,
+        elem_id=f"but-gallery_{i}",
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
+rows_tag = [
+    gr.Textbox(
+        value="",
+        label="Tag",
+        placeholder="empty",
+        visible=True,
+        elem_id=f"tag_{i}",
+        interactive=False,
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
 rows_tag_seq = {rows_tag[0]}
-for tag in rows_tag: rows_tag_seq.add(tag)
+for tag in rows_tag:
+    rows_tag_seq.add(tag)
 
-rows_action = [gr.Radio(choices=["delete", "replace", "deselect", "deselect_notags", "no_action", "undef"],value="undef",label="Action", visible=True, interactive=True) for i in range(TAG_NUM_PER_PAGE)]
-rows_payload = [gr.Textbox(value="",label="Payload",placeholder="empty", visible=True, interactive=True) for i in range(TAG_NUM_PER_PAGE)]
+rows_action = [
+    gr.Radio(
+        choices=[
+            "delete",
+            "replace",
+            "deselect",
+            "deselect_notags",
+            "no_action",
+            "undef",
+        ],
+        value="undef",
+        label="Action",
+        visible=True,
+        interactive=True,
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
+rows_payload = [
+    gr.Textbox(
+        value="", label="Payload", placeholder="empty", visible=True, interactive=True
+    )
+    for i in range(TAG_NUM_PER_PAGE)
+]
 
 pool: Pool = Pool()
+
 
 def hide_groups():
     return [gr.update(visible=False) for i in range(TAG_NUM_PER_PAGE)]
 
+
 def show_groups(page: int):
     return [gr.update(visible=True) for i in range(TAG_NUM_PER_PAGE)]
+
 
 def flush_tags():
     return [gr.update(value="") for i in range(TAG_NUM_PER_PAGE)]
 
-def new_pool(poolname:str):
+
+def new_pool(poolname: str):
     global pool
     if poolname == "None":
         pool = Pool()
@@ -65,10 +122,15 @@ def new_pool(poolname:str):
         gr.Info(f"Loading pool {poolname} ...")
     return
 
-def new_page_idx(page:int) -> None:
-    return [gr.update(value=TAG_NUM_PER_PAGE*(page-1) + i) for i in range(TAG_NUM_PER_PAGE)]
 
-def new_pool_page(poolname:str) -> None:
+def new_page_idx(page: int) -> None:
+    return [
+        gr.update(value=TAG_NUM_PER_PAGE * (page - 1) + i)
+        for i in range(TAG_NUM_PER_PAGE)
+    ]
+
+
+def new_pool_page(poolname: str) -> None:
     num = len(list(pool.tags.keys()))
     if num == 0:
         values = [1]
@@ -78,13 +140,17 @@ def new_pool_page(poolname:str) -> None:
     pages = num // per + 1
     if num % per > 0:
         pages += 1
-    values = [x for x in range(1,pages)]
+    values = [x for x in range(1, pages)]
     return gr.update(choices=values, value=values[0])
+
 
 def switch_gallery(switch: str, tag: str):
     OFF = LABEL_BUTT_GALLERY_OFF
     ON = LABEL_BUTT_GALLERY_ON
-    ret_off = (gr.update(value=OFF), gr.update(type="pil", value=[img_empty], visible=False))
+    ret_off = (
+        gr.update(value=OFF),
+        gr.update(type="pil", value=[img_empty], visible=False),
+    )
     ret_on = gr.update(value=ON)
     if switch == ON:
         return ret_off
@@ -94,10 +160,14 @@ def switch_gallery(switch: str, tag: str):
     urls = [pool[i]["img_url"] for i in tag_summary["selection"]]
     return ret_on, (gr.update(type="filepath", value=urls, visible=True))
 
+
 def switch_gallery_notags(switch: str, tag: str):
     OFF = LABEL_BUTT_GALLERY_OFF_NOTAGS
     ON = LABEL_BUTT_GALLERY_ON_NOTAGS
-    ret_off = (gr.update(value=OFF), gr.update(type="pil", value=[img_empty], visible=False))
+    ret_off = (
+        gr.update(value=OFF),
+        gr.update(type="pil", value=[img_empty], visible=False),
+    )
     ret_on = gr.update(value=ON)
     if switch == ON:
         return ret_off
@@ -110,10 +180,11 @@ def switch_gallery_notags(switch: str, tag: str):
     urls = [pool[i]["img_url"] for i in s_diff]
     return ret_on, (gr.update(type="filepath", value=urls, visible=True))
 
+
 def update_tags(page: int):
     tags = pool.tags_ordered
     size = TAG_NUM_PER_PAGE
-    start = (page-1) * size
+    start = (page - 1) * size
     end = start + size
 
     if start >= len(tags):
@@ -125,18 +196,28 @@ def update_tags(page: int):
 
     update = []
     for i, tag in enumerate(tags):
-        tags_summary: TagsSummary = pool.tags[tag] 
-        update.append(gr.update(value=tag, label=f"Tag [size: {len(tags_summary['selection'])}]"))
-    for j in range(i+1, TAG_NUM_PER_PAGE):
+        tags_summary: TagsSummary = pool.tags[tag]
+        update.append(
+            gr.update(value=tag, label=f"Tag [size: {len(tags_summary['selection'])}]")
+        )
+    for j in range(i + 1, TAG_NUM_PER_PAGE):
         update.append(gr.update(value="", label="Tag"))
-    
+
     return update
 
-def store_action(tag: str, action: Literal["delete", "replace", "deselect", "deselect_notags", "no_action", "undef"], payload: str):
+
+def store_action(
+    tag: str,
+    action: Literal[
+        "delete", "replace", "deselect", "deselect_notags", "no_action", "undef"
+    ],
+    payload: str,
+):
     tag_summary: TagsSummary = pool.tags[tag]
     tag_summary["action"]["action"] = action
     tag_summary["action"]["payload"] = payload
     pool.tags[tag] = tag_summary
+
 
 def restore_actions(tags: dict):
     update = []
@@ -146,6 +227,7 @@ def restore_actions(tags: dict):
         update.append(gr.update(value=value))
     return update
 
+
 def restore_payloads(tags: dict):
     update = []
     for tag in tags.values():
@@ -154,24 +236,40 @@ def restore_payloads(tags: dict):
         update.append(gr.update(value=value))
     return update
 
+
 def build_tags(trigger: str):
     gr.Info("Building tags ...")
     pool.build_tags_train(trigger)
     pool.safe_tags_train
     gr.Info("Building done!")
 
+
 with gr.Blocks() as interface:
-    #gr.Markdown("Template")
+    # gr.Markdown("Template")
     with gr.Row():
         labels = ["None"] + pools.poolnames
-        pools_select = gr.Dropdown(labels, label="Choose Pool", multiselect=False, value=labels[0],scale=2, interactive=True)
-        page_select = gr.Dropdown([1], label= "Page", multiselect=False, value=1, scale=2, interactive=True)
-        trigger = gr.Textbox(placeholder="Enter trigger tag", label="Trigger Tag", scale=2, interactive=True)
+        pools_select = gr.Dropdown(
+            labels,
+            label="Choose Pool",
+            multiselect=False,
+            value=labels[0],
+            scale=2,
+            interactive=True,
+        )
+        page_select = gr.Dropdown(
+            [1], label="Page", multiselect=False, value=1, scale=2, interactive=True
+        )
+        trigger = gr.Textbox(
+            placeholder="Enter trigger tag",
+            label="Trigger Tag",
+            scale=2,
+            interactive=True,
+        )
         btn_do_caption = gr.Button("Build Tags", scale=1)
 
     rows = []
     for i in range(TAG_NUM_PER_PAGE):
-        with gr.Row(visible=False,show_progress=True) as row:
+        with gr.Row(visible=False, show_progress=True) as row:
             rows.append(row)
             rows_tag[i].render()
             with gr.Column(scale=1):
@@ -183,21 +281,30 @@ with gr.Blocks() as interface:
                     rows_butt_galleries_notags[i].render()
                     rows_galleries[i].render()
 
-        rows_action[i].change(store_action, [rows_tag[i], rows_action[i], rows_payload[i]], None)     
-                
-        rows_butt_galleries[i].click(switch_gallery, [rows_butt_galleries[i] ,rows_tag[i]], [rows_butt_galleries[i], rows_galleries[i]])
-        rows_butt_galleries_notags[i].click(switch_gallery_notags, [rows_butt_galleries_notags[i] ,rows_tag[i]], [rows_butt_galleries_notags[i], rows_galleries[i]])
-    
-    pools_select.change(hide_groups,None, rows) \
-        .then(flush_tags,None, rows_tag) \
-        .then(new_pool,pools_select, None) \
-        .then(new_pool_page, pools_select, page_select) \
-        .then(update_tags, page_select, rows_tag) \
-        .then(restore_actions, rows_tag_seq, rows_action) \
-        .then(restore_payloads, rows_tag_seq, rows_payload) \
-        .then(show_groups,page_select,rows)
-    page_select.change(update_tags,page_select, rows_tag) \
-        .then(restore_actions, rows_tag_seq, rows_action) \
-        .then(restore_payloads, rows_tag_seq, rows_payload)
-    
+        rows_action[i].change(
+            store_action, [rows_tag[i], rows_action[i], rows_payload[i]], None
+        )
+
+        rows_butt_galleries[i].click(
+            switch_gallery,
+            [rows_butt_galleries[i], rows_tag[i]],
+            [rows_butt_galleries[i], rows_galleries[i]],
+        )
+        rows_butt_galleries_notags[i].click(
+            switch_gallery_notags,
+            [rows_butt_galleries_notags[i], rows_tag[i]],
+            [rows_butt_galleries_notags[i], rows_galleries[i]],
+        )
+
+    pools_select.change(hide_groups, None, rows).then(flush_tags, None, rows_tag).then(
+        new_pool, pools_select, None
+    ).then(new_pool_page, pools_select, page_select).then(
+        update_tags, page_select, rows_tag
+    ).then(restore_actions, rows_tag_seq, rows_action).then(
+        restore_payloads, rows_tag_seq, rows_payload
+    ).then(show_groups, page_select, rows)
+    page_select.change(update_tags, page_select, rows_tag).then(
+        restore_actions, rows_tag_seq, rows_action
+    ).then(restore_payloads, rows_tag_seq, rows_payload)
+
     btn_do_caption.click(build_tags, trigger, None)
