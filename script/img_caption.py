dde50d612523a489d6eeb5c6540bec2000062232
mikemoik <mikemoik@gmail.com>
2025-12-05 16:01:12 +0100

    .

--- script/img_caption.py
+++ script/img_caption.py
@@ -1,8 +1,11 @@
 import sys
+from pathlib import Path
 from ait.caption import Joy
 
 if __name__ == '__main__':
     url_img = sys.argv[1]
+
+    print(f'-> {Path(url_img).name}')
 
     joy = Joy('1gts')
     caption = joy.imgurl_caption(url_img)
--- script/img_prompt.py
+++ script/img_prompt.py
@@ -1,9 +1,13 @@
 import sys
+from pathlib import Path
 
 # from pprint import pprint
 from ait.image import get_prompt_comfy
 
 if __name__ == '__main__':
     url_img = sys.argv[1]
+
+    print(f'-> {Path(url_img).name}')
+
     prompt = get_prompt_comfy(url_img)
     print(prompt)
