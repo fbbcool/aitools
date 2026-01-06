c7fce3bdb29dcd86a909c7f5a815d9f22dff43a6
mikemoik <mikemoik@gmail.com>
2026-01-06 17:06:38 +0100

    .

--- 000_install/aitools.sh
+++ 000_install/aitools.sh
@@ -1,133 +1,14 @@
 #!/bin/sh
-export ENV_REPOS='https://github.com/fbbcool/aitools.git'
-if [[ -z $COMFYUI_DIR_INST ]]; then
-  export ENV_WORKSPACE='/workspace'
-else
-  export ENV_WORKSPACE=$COMFYUI_DIR_INST
-fi
-export ENV_HOME=$ENV_WORKSPACE/aitools
-export ENV_POST_HOOK=$ENV_WORKSPACE/post_install_hook.sh
-export PYTHONPATH=$ENV_HOME/src
+export AIT_REPOS='https://github.com/fbbcool/aitools.git'
+export WORKSPACE='/workspace'
+export AIT_HOME=$WORKSPACE/aitools
+export PYTHONPATH=$AIT_HOME/src
 
 ___install() {
-  rm -rf $ENV_HOME
-  git clone $ENV_REPOS $ENV_HOME
-  #pip install -r $ENV_HOME/requirements.txt
-  pip install -r $ENV_HOME/requirements_remote.txt
+  git clone $AIT_REPOS $AIT_HOME
+  pip install -r $AIT_HOME/requirements_remote.txt
 }
 ___update() {
   echo "update aitools ..."
-  git -C $ENV_HOME pull
-}
-___hook () {
-  if [ -x $ENV_POST_HOOK ]; then
-    $ENV_POST_HOOK
-  fi
-}
-___setup_qwen () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "qwen"
-}
-
-___setup_qwen_edit () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "qwen_edit"
-}
-
-___setup_wan21 () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "wan21"
-}
-
-___setup_wan22 () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "wan22"
-}
-
-___setup_wan22_i2v () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "wan22_i2v"
-}
-
-___current () {
-  ___update
-  python3 $ENV_HOME/script/vastai.py "current"
-}
-
-___wan21 () {
-  ___install
-  ___setup_wan21
-  ___hook
-}
-___wan22 () {
-  ___install
-  ___setup_wan22
-  ___hook
-}
-___wan22_i2v () {
-  ___install
-  ___setup_wan22_i2v
-  ___hook
-}
-___qwen () {
-  ___install
-  ___setup_qwen
-  ___hook
-}
-
-___qwen_edit () {
-  ___install
-  ___setup_qwen_edit
-  ___hook
-}
-
-___sd15 () {
-  git -C $ENV_HOME pull
-  comfyui_sd15
-}
-
-___sdxl () {
-  git -C $ENV_HOME pull
-  comfyui_sdxl
-}
-
-___sdall () {
-  git -C $ENV_HOME pull
-  comfyui_sdall
-}
-
-___flux () {
-  git -C $ENV_HOME pull
-  comfyui_flux
-}
-
-___flux_refine () {
-  git -C $ENV_HOME pull
-  comfyui_flux_refine
-}
-
-___hidream () {
-  git -C $ENV_HOME pull
-  comfyui_hidream
-}
-
-___fluxgym () {
-  git -C $ENV_HOME pull
-  comfyui_fluxgym
-}
-
-___kohyass_flux () {
-  git -C $ENV_HOME pull
-  comfyui_kohyass_flux
-}
-
-___kohyass_sdxl () {
-  git -C $ENV_HOME pull
-  comfyui_kohyass_sdxl
-}
-
-___clean_gen ()
-{
-  rm -rf $ENV_WORKSPACE/ComfyUI/output/video
-  rm -rf $ENV_WORKSPACE/ComfyUI/temp/*
+  git -C $AIT_HOME pull
 }
