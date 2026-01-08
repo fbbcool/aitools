0db04fdd5f25c7cb347de266fab9bd62c6cf5406
mikemoik <mikemoik@gmail.com>
2026-01-08 13:07:03 +0100

    .

--- 000_install/aitools.sh
+++ 000_install/aitools.sh
@@ -1,43 +1,46 @@
 #!/bin/sh
 export REPOS_AIT='https://github.com/fbbcool/aitools.git'
+export REPOS_TRAINER='https://github.com/tdrussell/diffusion-pipe'
+#export REPOS_TRAINER='https://github.com/fbbcool/diffusion-pipe'
 export WORKSPACE='/workspace'
+export HOME_TRAINER=$WORKSPACE/diffusion-pipe
 export HOME_AIT=$WORKSPACE/aitools
 export CONF_AIT=$HOME_AIT/conf
 export PYTHONPATH=$HOME_AIT/src
 
-train_install() {
-  rm -rf $WORKSPACE/diffusion-pipe
-  git clone https://github.com/tdrussell/diffusion-pipe $WORKSPACE/diffusion-pipe
-  #git clone https://github.com/fbbcool/diffusion-pipe $WORKSPACE/diffusion-pipe
-  cd $WORKSPACE/diffusion-pipe
+___train_install_aitools() {
+  git clone $REPOS_AIT $HOME_AIT
+  pip install -r $HOME_AIT/requirements_remote.txt
+}
+___train_install_trainer() {
+  rm -rf $HOME_TRAINER
+  git clone $REPOS_TRAINER $HOME_TRAINER
+  cd $HOME_TRAINER
   git submodule update --init --recursive
 
   pip install --no-build-isolation flash-attn>=2.8.3
   pip install -r requirements.txt
 
   pip uninstall diffusers
-  pip install git+https://github.com/huggingface/diffusers
-
-  git -C $HOME_AIT pull
+  yes | pip install git+https://github.com/huggingface/diffusers
+}
+train_install() {
+  ___train_install_aitools
+  ___train_install_trainer
 }
 train_prepare() {
   python $HOME_AIT/script/train_prepare.py
-  cp $WORKSPACE/train/train.sh $WORKSPACE/diffusion-pipe
+  cp $WORKSPACE/train/train.sh $HOME_TRAINER
 }
 train_run() {
   train_prepare
-  cd $WORKSPACE/diffusion-pipe
+  cd $HOME_TRAINER
   ./train.sh
 }
 
 alias t='vi $HOME_AIT/script/train_prepare.py'
 
-___install() {
-  git clone $REPOS_AIT $HOME_AIT
-  pip install -r $HOME_AIT/requirements_remote.txt
-  train_install
-}
-___update() {
+ait_pull() {
   echo "update aitools ..."
   git -C $HOME_AIT pull
 }
