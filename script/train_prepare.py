1ba4ae2f38d28d3a57d9298c15f4e32d07d8c6c2
mikemoik <mikemoik@gmail.com>
2026-06-24 14:05:49 +0200

    .

--- script/train_prepare.py
+++ script/train_prepare.py
@@ -31,7 +31,7 @@ model = 'qwen'
 variant = 'gts-app'
 gpu = '5090'
 # gpu = 'h100-nvl'
-trigger = 'xlleggy'
+trigger = 'xlfbb'
 num_repeats = 1
 
 # ──────────────────────────────────────────────────────
@@ -103,7 +103,7 @@ config_trainer_qwen_gts_app = {
     'checkpoint_every_n_epochs': 1,
     'caching_batch_size': 4,
     'steps_per_print': 10,
-    'adapter___rank': 16,  # 32 for xlasm, 16 for xlasm-childs
+    'adapter___rank': 4,  # 32 for xlasm, 16 for xlasm-childs
     #'adapter___alpha': 4,  # will break; is set automatically!
     'optimizer___lr': 5e-5,
 }
