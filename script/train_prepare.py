97fa5a8699aeb14fb028e051173662248c5c7b8d
mikemoik <mikemoik@gmail.com>
2026-09-25 22:36:02 +0200

    .

--- script/train_prepare.py
+++ script/train_prepare.py
@@ -184,7 +184,7 @@ config_trainer_krea2_gts_app_atomic = {
     'checkpoint_every_n_epochs': 1,
     'caching_batch_size': 4,
     'steps_per_print': 10,
-    'adapter___rank': 8,  # 32 for xlasm, 16 for xlasm-childs
+    'adapter___rank': 32,  # 32 for xlasm, 16 for xlasm-childs
     #'adapter___alpha': 4,  # will break; is set automatically!
     'optimizer___lr': 1e-4,
 }
